1230371979a85819410e4225932f7af734cbda08
varunbgit <vbansalevents01@gmail.com>
2021-09-09 17:39:00 +0530

    Very important concept of String

---
+++ size of string VERY IMP.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main()
+{
+	string str;
+	str="varun";
+	cout<<"size of one element of string is "<<sizeof(str[0])<<endl;;
+	cout<<"size of entire string is"<<sizeof(str);
+
+}
